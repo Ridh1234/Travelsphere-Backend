28271afdb3c372844d8d745af4d2ffd171ab6af3
Hridyansh173 <sridh429@gmail.com>
2024-07-08 22:07:43 +0530

    "changed path"

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const tripRoutes = require('./src/Routes/tripRoutes');
+const userRoutes = require('./src/Routes/userRoutes');
+const connectDB = require('./src/config/db');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors(
+  {
+    methods:["POST","GET"],
+    credentials:true
+  }
+));
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+app.get('/api/config', (req, res) => res.send('API is running...'));
+
+// Routes
+app.use('/api/trips', tripRoutes);
+app.use('/api/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
